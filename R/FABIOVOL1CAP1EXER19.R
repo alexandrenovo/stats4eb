6ef2acedd7eb3dea58dff5f5880df2da82a506df
alexandrenovo <alexandre.f@colsantana.com.br>
2021-05-19 18:09:00 -0300

    VERSAO2

---
+++ R/FABIOVOL1CAP1EXER19.R
@@ -0,0 +1,10 @@
+#' FABIOVOL1CAP1EXER19
+#'
+#' POPULACAO RESIDENTE NO BRASIL
+#'
+#' @format A data frame with 7 rows and 2 variables:
+#' \describe{
+#'   \item{DATA}{DATE}
+#'   \item{POPULACAO}{Population of Brazil.}
+#' }
+"FABIOVOL1CAP1EXER19"
---
+++ R/FABIOVOL1CAP1EXER22.R
@@ -0,0 +1,11 @@
+#' FABIOVOL1CAP1EXER22
+#'
+#' HORAS DE USO DE FONE DE OUVIDO
+#'
+#' @format A data frame with 8 rows and 2 variables:
+#' \describe{
+#'   \item{TEMPO}{TEMPO EM HORAS}
+#'   \item{FREQUENCIA}{FREQUENCIA EM PERCENTUAL}
+#'
+#' }
+"FABIOVOL1CAP1EXER22"
