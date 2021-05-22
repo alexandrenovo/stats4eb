42cc631e141d55bef79bee383a08c327426c8ac3
fsbmat-ufv <fernando.bastos@ufv.br>
2021-05-22 10:52:24 -0300

    description

--- inst/shinyApp/stats4eb_app/ArquivosServer/EF1Colunas.R
+++ inst/shinyApp/stats4eb_app/ArquivosServer/EF1Colunas.R
@@ -29,28 +29,28 @@ output$plot1 <- renderPlotly({
   rm(list=ls())
   if(input$vetornome1=="ADILSONVOL2CAP6EXE1"){
     data <- ADILSONVOL2CAP6EXE1
-    plot1 <- ggplot(data, aes(ANO, `RENDIMENTO MEDIO`, fill=ANO)) +
-      geom_col(show.legend = FALSE) +
-      ggtitle("RENDIMENTO MÉDIO EM REAIS") +
-      scale_x_continuous(breaks=seq(1997,2007, length=11)) +
-      theme_minimal()
+    plot1 <- ggplot2::ggplot(data, aes(ANO, `RENDIMENTO MEDIO`, fill=ANO)) +
+      ggplot2::geom_col(show.legend = FALSE) +
+      ggplot2::ggtitle("RENDIMENTO MÉDIO EM REAIS") +
+      ggplot2::scale_x_continuous(breaks=seq(1997,2007, length=11)) +
+      ggplot2::theme_minimal()
   } else {
     if(input$vetornome1=="ADILSONVOL2CAP6EXE2"){
       data <- ADILSONVOL2CAP6EXE2
-      plot1 <- ggplot(ADILSONVOL2CAP6EXE2, aes(CLUBE, `PERCENTUAL DE TORCEDORES`, fill=CLUBE)) +
-        geom_col(show.legend = FALSE) +
-        ggtitle("PERCENTUAL DE TORCEDORES") +
-        theme_minimal()
+      plot1 <- ggplot2::ggplot(ADILSONVOL2CAP6EXE2, aes(CLUBE, `PERCENTUAL DE TORCEDORES`, fill=CLUBE)) +
+        ggplot2::geom_col(show.legend = FALSE) +
+        ggplot2::ggtitle("PERCENTUAL DE TORCEDORES") +
+        ggplot2::theme_minimal()
     } else {
       data <- ADILSONVOL2CAP20EXEMP1
-      plot1 <- ggplot(data, aes(LOCAL, `TAXA DE MORTALIDADE`, fill=LOCAL)) +
-        geom_col(show.legend = FALSE) +
-        ggtitle("TAXA DE MORTALIDADE") +
-        theme_minimal()+
-        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+      plot1 <- ggplot2::ggplot(data, aes(LOCAL, `TAXA DE MORTALIDADE`, fill=LOCAL)) +
+        ggplot2::geom_col(show.legend = FALSE) +
+        ggplot2::ggtitle("TAXA DE MORTALIDADE") +
+        ggplot2::theme_minimal()+
+        ggplot2::theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
     }
   }
 
-  ggplotly(plot1) %>% layout(showlegend = FALSE) %>% style(textposition = "top")
+  plotly::ggplotly(plot1) %>% plotly::layout(showlegend = FALSE) %>% plotly::style(textposition = "top")
 
 })
